dbc2665c6b979124ad18fdb244adbb1b7ef399bb
Jellyfishcoder <jellyfishcoder@gmail.com>
2016-11-11 10:26:21 -0600

    Added main menu function and partial functionality

---
+++ include/main.h
@@ -0,0 +1,19 @@
+// Main header file
+#ifndef MAIN_H
+#define MAIN_H
+// nDS Libraries
+#include <nds.h>
+#include <nds/ndstypes.h>
+#include <nds/arm9/sassert.h>
+// Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+// Background Headers
+//#include "menuscreen.h"
+// Custom Libraries
+#include "inits.h"
+
+// Functions Referances
+void mainMenu();
+
+#endif
